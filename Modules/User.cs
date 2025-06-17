099324664ec5eec1cc05403c88fa3883a9bdb413
Hemanthsai399 <hemanthsai399@gmail.com>
2025-06-17 20:56:19 +0530

    firstone

--- Modules/User.cs
+++ Modules/User.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace SmartAppointmentBookingSystem.Modules
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+
     }
 }
--- obj/Debug/net6.0/SmartAppointmentBookingSystem.AssemblyInfo.cs
+++ obj/Debug/net6.0/SmartAppointmentBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartAppointmentBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb6b046631a17025931a4b5acbbde5a4243945b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartAppointmentBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartAppointmentBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
